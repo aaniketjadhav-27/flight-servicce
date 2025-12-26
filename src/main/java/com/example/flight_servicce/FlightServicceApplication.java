965bdf2d158b79510365c3237cfe78958b103278
aaniketjadhav <aaniketjadhav63@gmail.com>
2025-12-26 17:30:45 +0530

    code refactor

---
+++ src/main/java/com/example/flight_servicce/FlightServicceApplication.java
@@ -0,0 +1,13 @@
+package com.example.flight_servicce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightServicceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightServicceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flight_servicce/FlightServicceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flight_servicce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightServicceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
